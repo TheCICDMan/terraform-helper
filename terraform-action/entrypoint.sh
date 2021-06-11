f181efdaf69613d104faeceaac6ecc0a05e52067
David Hoffman <david.hoffman@vertexinc.com>
2021-06-10 20:35:07 -0500

    Testing

--- terraform-action/entrypoint.sh
+++ terraform-action/entrypoint.sh
@@ -1,18 +1,26 @@
 #!/bin/bash -xe
-env
+export K8S_ENV=${INPUT_K8S_ENV}
 
+echo "============================================================="
+echo "=                                                           ="
+echo "=           Create GitHub Credentials                       ="
+echo "=                                                           ="
+echo "============================================================="
 git config --global credential.helper store
 echo "https://${INPUT_GITHUB_USER}:${INPUT_GITHUB_TOKEN}@github.com/vertexinc" > ~/.git-credentials
 git config --global --replace-all url.https://github.com/.insteadOf ssh://git@github.com/
 git config --global --add url.https://github.com/.insteadOf git@github.com:
 
+echo "============================================================="
+echo "=                                                           ="
+echo "=           Create AWS Credentials File                     ="
+echo "=                                                           ="
+echo "============================================================="
 mkdir -p ~/.aws
 echo "[${INPUT_AWS_PROFILE}]" > ~/.aws/credentials
 echo "region: ${INPUT_AWS_REGION}" >> ~/.aws/credentials
 echo "aws_access_key_id=${INPUT_AWS_ACCESS_KEY}" >> ~/.aws/credentials
 echo "aws_secret_access_key=${INPUT_AWS_SECRET_KEY}" >> ~/.aws/credentials
-
-ls -ltr ${GITHUB_WORKSPACE}
 
 cd ${GITHUB_WORKSPACE}/${INPUT_DEPLOYMENT_TYPE}/${INPUT_K8S_ENV}
 
@@ -50,4 +58,3 @@ echo "=           Execute                                           ="
 echo "=                                                           ="
 echo "============================================================="
 eval "${INPUT_EXECUTE_COMMAND}"
-

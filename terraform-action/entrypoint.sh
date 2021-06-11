9abc5d6483ba6428b0c1806d0bb0360d2ea19e68
David Hoffman <david.hoffman@vertexinc.com>
2021-06-10 19:43:49 -0500

    Testing

--- terraform-action/entrypoint.sh
+++ terraform-action/entrypoint.sh
@@ -2,19 +2,19 @@
 env
 
 git config --global credential.helper store
-echo "https://${CFS_K8S_GITHUB_USER}:${CFS_K8S_GITHUB_TOKEN}@github.com/vertexinc" > ~/.git-credentials
+echo "https://$3:$4@github.com/vertexinc" > ~/.git-credentials
 git config --global --replace-all url.https://github.com/.insteadOf ssh://git@github.com/
 git config --global --add url.https://github.com/.insteadOf git@github.com:
 
 mkdir -p ~/.aws
-echo "[${AWS_PROFILE}]" > ~/.aws/credentials
-echo "region: ${AWS_REGION}" >> ~/.aws/credentials
-echo "aws_access_key_id=${CFS_K8S_AWS_ACCESS_KEY}" >> ~/.aws/credentials
-echo "aws_secret_access_key=${CFS_K8S_AWS_SECRET_KEY_DEV}" >> ~/.aws/credentials
+echo "[$5]" > ~/.aws/credentials
+echo "region: $7" >> ~/.aws/credentials
+echo "aws_access_key_id=$7" >> ~/.aws/credentials
+echo "aws_secret_access_key=$8" >> ~/.aws/credentials
 
-ls -ltr ${GITHUB_WORKSPACE}
+ls -ltr $1
 
-cd ${GITHUB_WORKSPACE}/${DEPLOYMENT_TYPE}/${K8S_ENV}
+cd $1/$2/$9
 
 echo "============================================================="
 echo "=                                                           ="
@@ -42,11 +42,12 @@ echo "=                                                           ="
 echo "=           Update kubeconfig                               ="
 echo "=                                                           ="
 echo "============================================================="
-aws eks update-kubeconfig --name cfs-${K8S_ENV}-eks --kubeconfig .terraform/cfs/output/eks_kubeconfig.yaml
+aws eks update-kubeconfig --name cfs-$1-eks --kubeconfig .terraform/cfs/output/eks_kubeconfig.yaml
 
 echo "============================================================="
 echo "=                                                           ="
 echo "=           Execute                                           ="
 echo "=                                                           ="
 echo "============================================================="
-eval "${EXECUTE_COMMAND}"
+eval "${10}"
+
